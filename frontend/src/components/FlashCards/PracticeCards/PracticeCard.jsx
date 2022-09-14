50bea37459863a065dfb5277a9c20583de662d07
Patrick Nepp <ptnepp@gmail.com>
2022-09-13 22:15:13 -0500

    changes to practice card modals

--- frontend/src/components/FlashCards/PracticeCards/PracticeCard.jsx
+++ frontend/src/components/FlashCards/PracticeCards/PracticeCard.jsx
@@ -1,19 +1,36 @@
-import { Box, Container, Divider, Typography } from '@mui/material'
+import { AppBar, Box, Container, Divider, Toolbar, Typography } from '@mui/material'
 import React from 'react'
-import data from '../../../data'
+import { useState } from 'react'
+import { data } from '../../../data'
 import CommonButton from '../../common/CommonButton'
 
 const PracticeCard = () => {
+    const stuff = data[0]
+    const [num, setNum] = useState(0)
+    console.log(stuff.subjectWords[num].english)
+
   return (
     <Container>
+        <AppBar position='static'><Toolbar sx={{display: 'flex', justifyContent:'center'}}><Typography>Fruit</Typography></Toolbar></AppBar>
         <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
-            <Typography bgcolor='red' p={5}></Typography>
+            <Typography p={5}>{stuff.subjectWords[num].english}</Typography>
             <Divider orientation='vertical' variant='middle' flexItem/>
-            <Typography bgcolor='blue' p={5}></Typography>
+            <Typography p={5}>{stuff.subjectWords[num].translated}</Typography>
         </Box>
-        <Box>
-            <CommonButton>Back</CommonButton>
-            <CommonButton>Next</CommonButton>
+        <Box display='flex' justifyContent='center'>
+            <CommonButton
+                color='warning' 
+                disabled={num === 0 && true}
+                onClick={() => setNum(num - 1)}
+            >
+                Back
+            </CommonButton>
+            <CommonButton 
+                color='success'
+                onClick={() => setNum(num + 1)}
+            >
+                {num === stuff.subjectWords.length - 1 ? 'Finish' : 'Next'} 
+            </CommonButton>
         </Box>
     </Container>
   )
